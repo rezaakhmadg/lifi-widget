6ffbce305ab86e256fad8727f73f5952ef61a505
Reza Gandara <rezagandara@Rezas-Mac-mini.local>
2025-08-03 08:40:43 +0700

    Test SOL

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { LiFiWidget } from '@lifi/widget'
-
+import { LiFiWidget } from '@lifi/widget';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+ 
 export function App() {
+  const wallets = [new PhantomWalletAdapter()];
+ 
   return (
-    <LiFiWidget
-      integrator="Liminal-Stables"
-      config={{
-        theme: {
-          container: {
-            border: '1px solid rgb(234, 234, 234)',
-            borderRadius: '16px',
-          },
-        },
-      }}
-    />
-  )
+    <ConnectionProvider endpoint={clusterApiUrl('mainnet-beta')}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <LiFiWidget
+          integrator="Liminal-Stables"
+          config={{
+            appearance: 'dark',
+            chains: 'all',
+            solana: {
+              enabled: true,
+            },
+            theme: {
+              container: {
+                border: '1px solid rgb(234, 234, 234)',
+                borderRadius: '16px',
+              },
+            },
+          }}
+        />
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
